35e56d59e95877e82f295e26c81914fdf07ded55
Byrne Hollander <bhollander823@gmail.com>
2019-01-28 21:43:01 -0500

    restart git

---
+++ precache-manifest.57400db16fad3957b9d6c264e272e2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "570465a1a2e0d0411dcf",
+    "url": "/static/js/main.570465a1.chunk.js"
+  },
+  {
+    "revision": "2de6dc739e7cfdae2122",
+    "url": "/static/js/1.2de6dc73.chunk.js"
+  },
+  {
+    "revision": "570465a1a2e0d0411dcf",
+    "url": "/static/css/main.f832746a.chunk.css"
+  },
+  {
+    "revision": "2de6dc739e7cfdae2122",
+    "url": "/static/css/1.fa4985b6.chunk.css"
+  },
+  {
+    "revision": "583c8495492e045bbf2469f7747af7e8",
+    "url": "/index.html"
+  }
+];

bd2a1c2c90e07fca0e966eb707256e4b1995b26d
Byrne Hollander <bhollander823@gmail.com>
2019-03-19 17:36:07 -0400

    Updates

---
+++ precache-manifest.7d3f569242790cfd868823bac0c01e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11e14c93a56b8c9e6070",
+    "url": "/static/js/main.11e14c93.chunk.js"
+  },
+  {
+    "revision": "012b0b875979f1e30f8d",
+    "url": "/static/js/1.012b0b87.chunk.js"
+  },
+  {
+    "revision": "11e14c93a56b8c9e6070",
+    "url": "/static/css/main.1a2f6bee.chunk.css"
+  },
+  {
+    "revision": "012b0b875979f1e30f8d",
+    "url": "/static/css/1.deb2527a.chunk.css"
+  },
+  {
+    "revision": "83eca2d1d15d11fdbc321875ddcc1a13",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cfe57f8b99da93b5c1a983d337de751.js"
+  "/precache-manifest.7d3f569242790cfd868823bac0c01e30.js"
 );
 
 workbox.clientsClaim();

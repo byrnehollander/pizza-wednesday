eb70d7fb387e131e4801b9b42a4c7bbabecd559f
Byrne Hollander <bhollander823@gmail.com>
2019-07-28 21:56:06 -0400

    Updates

--- precache-manifest.fafbce53488d78ce5e22e9d630b3bafe.js
+++ precache-manifest.fafbce53488d78ce5e22e9d630b3bafe.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "c0ae5b20e476bd4c9f5f",
-    "url": "/static/js/main.c0ae5b20.chunk.js"
+    "revision": "55fc2a7cc4db1fca7b5b",
+    "url": "/static/js/main.55fc2a7c.chunk.js"
   },
   {
     "revision": "2de6dc739e7cfdae2122",
     "url": "/static/js/1.2de6dc73.chunk.js"
   },
   {
-    "revision": "c0ae5b20e476bd4c9f5f",
-    "url": "/static/css/main.f832746a.chunk.css"
+    "revision": "55fc2a7cc4db1fca7b5b",
+    "url": "/static/css/main.a0cf04ae.chunk.css"
   },
   {
     "revision": "2de6dc739e7cfdae2122",
     "url": "/static/css/1.fa4985b6.chunk.css"
   },
   {
-    "revision": "c7aaba3bd369680e0752c86568f9b835",
+    "revision": "8cc9cd4814f453438d3585bb92f6666e",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e64b0945e97d19ed090a679f934c39f1.js"
+  "/precache-manifest.fafbce53488d78ce5e22e9d630b3bafe.js"
 );
 
 workbox.clientsClaim();

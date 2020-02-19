bfda6d311badac7f29ff0b51c3db95debb98b8c5
Byrne Hollander <bhollander@seatgeek.com>
2020-02-19 18:09:58 -0500

    Updates

---
+++ precache-manifest.966403da05eb99977578206fe3c02861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7d004e5c10f06c017a7",
+    "url": "/static/js/main.a7d004e5.chunk.js"
+  },
+  {
+    "revision": "2de6dc739e7cfdae2122",
+    "url": "/static/js/1.2de6dc73.chunk.js"
+  },
+  {
+    "revision": "a7d004e5c10f06c017a7",
+    "url": "/static/css/main.f9540481.chunk.css"
+  },
+  {
+    "revision": "2de6dc739e7cfdae2122",
+    "url": "/static/css/1.fa4985b6.chunk.css"
+  },
+  {
+    "revision": "e95a3b543c51eff15239a84fd7c24659",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c60555b213949589618733c72475b33b.js"
+  "/precache-manifest.966403da05eb99977578206fe3c02861.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

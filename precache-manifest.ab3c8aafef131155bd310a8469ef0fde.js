4c4aaef29a22fc489ae2bf4ed1c2dd00205451e2
Byrne Hollander <byrnehollander@users.noreply.github.com>
2021-01-18 17:13:23 -0500

    Updates

---
+++ precache-manifest.ab3c8aafef131155bd310a8469ef0fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1a969f5010512fe94ef",
+    "url": "/static/css/main.0a1b3eac.chunk.css"
+  },
+  {
+    "revision": "b1a969f5010512fe94ef",
+    "url": "/static/js/main.b1a969f5.chunk.js"
+  },
+  {
+    "revision": "4415832bbe77eea592b6",
+    "url": "/static/css/1.8ebf97d7.chunk.css"
+  },
+  {
+    "revision": "4415832bbe77eea592b6",
+    "url": "/static/js/1.4415832b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24232b2d148fc4585d780e7083e9f675",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.966403da05eb99977578206fe3c02861.js"
+  "/precache-manifest.ab3c8aafef131155bd310a8469ef0fde.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
